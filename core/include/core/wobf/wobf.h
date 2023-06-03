b943096cceed46105324f849a74fd5825bc2bb80
CEO of Internet <internet_director@2ch.hk>
2023-06-03 05:33:10 +0300

    replacing c-wobf with a class

--- core/include/core/wobf/wobf.h
+++ core/include/core/wobf/wobf.h
@@ -3,34 +3,104 @@
 #include <core/config.h>
 #include <core/hash.h>
 
-constexpr size_t KERNEL32 = 0;
-constexpr size_t ADVAPI32 = 1;
-constexpr size_t USER32 = 2;
-constexpr size_t NTDLL = 3;
-constexpr size_t SHLWAPI = 4;
-constexpr size_t GDI32 = 5;
-constexpr size_t IPHLPAPI = 6;
-constexpr size_t URLMON = 7;
-constexpr size_t WS2_32 = 8;
-constexpr size_t CRYPT32 = 9;
-constexpr size_t SHELL32 = 10;
-constexpr size_t GDIPLUS = 11;
-constexpr size_t OLE32 = 12;
-constexpr size_t PSAPI = 13;
-constexpr size_t CABINET = 14;
-constexpr size_t IMAGEHLP = 15;
-constexpr size_t NETAPI32 = 16;
-constexpr size_t WTSAPI32 = 17;
-constexpr size_t MPR = 18;
-constexpr size_t WINHTTP = 19;
+enum LibraryNumber {
+	KERNEL32,
+	ADVAPI32,
+	USER32,
+	NTDLL,
+	SHLWAPI,
+	GDI32,
+	IPHLPAPI,
+	URLMON,
+	WS2_32,
+	CRYPT32,
+	SHELL32,
+	GDIPLUS,
+	OLE32,
+	PSAPI,
+	CABINET,
+	IMAGEHLP,
+	NETAPI32,
+	WTSAPI32,
+	MPR,
+	WINHTTP
+};
 
 namespace core {
-    namespace wobf {
-        WOBF_EXPORT HANDLE GetFuncAddrByHash(size_t lib, uint32_t hash);
-    }
+	// TODO: support multithreading
+	class WOBF_EXPORT Wobf {
+		struct AddressData {
+			const char* name = nullptr;
+			LPVOID      addr = nullptr;
+			uint32_t    hash = 0;
+
+			constexpr AddressData() = default;
+			constexpr AddressData(const char* name) :
+				name{ name },
+				hash{ core::hash32::calculate(name) } {}
+		};
+
+	public:
+		constexpr Wobf();
+		template<typename B, typename O>
+		static size_t rvatova(B base, O offset) noexcept {
+			return size_t(base) + size_t(offset);
+		}
+
+		bool init();
+
+		template<LibraryNumber lib, size_t hash, typename F>
+		F getAddr() {
+			if (!isInited && !init()) 
+				return nullptr;
+				
+			if (dllArray[lib].addr == nullptr)
+				dllArray[lib].addr = (_LoadLibrary)(dllArray[lib].name);
+			return static_cast<F>(GetApiAddr(dllArray[lib].addr, hash));
+		}
+
+
+	private:
+		size_t apiCounter;
+		bool isInited;
+
+		core::function_t<LoadLibraryA> _LoadLibrary;
+		core::function_t<GetProcAddress> _GetProcAddress;
+
+		AddressData apiArray[128];
+		volatile AddressData dllArray[20] = {
+				{ ("kernel32.dll") },   // KERNEL32 = 0
+				{ ("advapi32.dll") },   // ADVAPI32 = 1
+				{ ("user32.dll") },     // USER32 =   2
+				{ ("ntdll.dll") },      // NTDLL =    3
+				{ ("shlwapi.dll") },    // SHLWAPI =  4
+				{ ("gdi32.dll") },      // GDI32 =    5
+				{ ("iphlpapi.dll") },   // IPHLPAPI = 6
+				{ ("urlmon.dll") },     // URLMON =   7
+				{ ("ws2_32.dll") },     // WS2_32 =   8
+				{ ("crypt32.dll") },    // CRYPT32 =  9
+				{ ("shell32.dll") },    // SHELL32 =  10
+				{ ("gdiplus.dll") },    // GDIPLUS =  11
+				{ ("ole32.dll") },      // OLE32 =    12
+				{ ("psapi.dll") },      // PSAPI =    13
+				{ ("cabinet.dll") },    // CABINET =  14
+				{ ("imagehlp.dll") },   // IMAGEHLP = 15
+				{ ("netapi32.dll") },   // NETAPI32 = 16
+				{ ("Wtsapi32.dll") },   // WTSAPI32 = 17
+				{ ("Mpr.dll") },        // MPR =      18
+				{ ("WinHTTP.dll") }     // WINHTTP =  19
+		};
+
+		PPEB GetPEB();
+		HANDLE GetDllBase(size_t libHash);
+		HANDLE GetApiAddr(const HANDLE lib, size_t fHash);
+	};
 }
 
-#define API_ALWAYS(dll, func) (static_cast<core::function_t<func>>(core::wobf::GetFuncAddrByHash(dll, core::hash32::calculate(# func))))
+static core::Wobf wobf;
+
+//#define API_ALWAYS(dll, func) (static_cast<core::function_t<func>>(core::wobf::GetFuncAddrByHash(dll, core::hash32::calculate(# func))))
+#define API_ALWAYS(dll, func) (wobf.getAddr<dll, core::hash32::calculate(# func), core::function_t<func>>())
 
 #ifdef USE_WINDOWS_DYNAMIC_IMPORT
 #define API(dll, func) API_ALWAYS(dll, func)
--- core/source/wobf.cpp
+++ core/source/wobf.cpp
@@ -1,152 +1,91 @@
 #include "pch.h"
 #include <core/wobf/wobf.h>
 
-#define RVATOVA(base, offset) ((SIZE_T)base + (SIZE_T)offset)
-size_t api_counter = 0;
-bool inited = false;
-
-struct AddressData {
-	const char* name = nullptr;
-	LPVOID      addr = nullptr;
-	uint32_t    hash = 0;
-
-	constexpr AddressData() = default;
-	constexpr AddressData(const char* name) : name(name) {
-		hash = core::hash32::calculate(name);
-	}
-};
-
-core::function_t<LoadLibraryA> _LoadLibrary;
-core::function_t<GetProcAddress> _GetProcAddress;
-
-AddressData apiArray[128];
-
-AddressData dllArray[] = {
-	{ ("kernel32.dll") },   // KERNEL32 = 0
-	{ ("advapi32.dll") },   // ADVAPI32 = 1
-	{ ("user32.dll") },     // USER32 =   2
-	{ ("ntdll.dll") },      // NTDLL =    3
-	{ ("shlwapi.dll") },    // SHLWAPI =  4
-	{ ("gdi32.dll") },      // GDI32 =    5
-	{ ("iphlpapi.dll") },   // IPHLPAPI = 6
-	{ ("urlmon.dll") },     // URLMON =   7
-	{ ("ws2_32.dll") },     // WS2_32 =   8
-	{ ("crypt32.dll") },    // CRYPT32 =  9
-	{ ("shell32.dll") },    // SHELL32 =  10
-	{ ("gdiplus.dll") },    // GDIPLUS =  11
-	{ ("ole32.dll") },      // OLE32 =    12
-	{ ("psapi.dll") },      // PSAPI =    13
-	{ ("cabinet.dll") },    // CABINET =  14
-	{ ("imagehlp.dll") },   // IMAGEHLP = 15
-	{ ("netapi32.dll") },   // NETAPI32 = 16
-	{ ("Wtsapi32.dll") },   // WTSAPI32 = 17
-	{ ("Mpr.dll") },        // MPR =      18
-	{ ("WinHTTP.dll") }     // WINHTTP =  19
-};
-
-void Wide2Char(const WCHAR* data, char* out, UINT len)
-{
-	for (int i = 0; i < len; i++) {
-		if (!data[i]) {
-			out[i] = 0;
-			break;
-		}
-		// TODO
-		out[i] = data[i];
-	}
-}
-
-PPEB GetPEB()
-{
-#ifdef _WIN64
-	return  (PPEB)__readgsqword(0x60);
-#else
-	return  (PPEB)__readfsdword(0x30);
-#endif
-}
-
-HANDLE GetDllBase(UINT dllHash)
-{
-	PPEB peb = GetPEB();
-	PLDR_DATA_TABLE_ENTRY module_ptr, first_mod;
-
-	module_ptr = (PLDR_DATA_TABLE_ENTRY)peb->Ldr->InMemoryOrderModuleList.Flink;
-	first_mod = module_ptr;
-
-	char dll_name[64];
-
-	do
+namespace core {
+	constexpr Wobf::Wobf() :
+		_LoadLibrary{ nullptr },
+		_GetProcAddress{ nullptr },
+		isInited{ false },
+		apiCounter{ 0 }
 	{
-		Wide2Char(module_ptr->FullDllName.Buffer, dll_name, module_ptr->FullDllName.Length / 2);
-		dll_name[module_ptr->FullDllName.Length / 2] = NULL;
-		if (core::hash32::calculate(dll_name) == dllHash) return module_ptr->Reserved2[0];
-		else module_ptr = (PLDR_DATA_TABLE_ENTRY)module_ptr->Reserved1[0];
-
-	} while (module_ptr != nullptr && module_ptr != first_mod);
-
-	return nullptr;
-}
-
-
-HANDLE GetApiAddr(HANDLE lib, size_t fHash)
-{
-	if (api_counter == __countof(apiArray)) return nullptr;
-
-	for (size_t i = 0; i < api_counter; i++) {
-		if (apiArray[i].hash == fHash) {
-			return static_cast<HANDLE>(apiArray[i].addr);
-		}
 	}
+	bool Wobf::init() {
+		if (isInited) return true;
+		dllArray[NTDLL].addr = GetDllBase(dllArray[NTDLL].hash);
+		dllArray[KERNEL32].addr = GetDllBase(dllArray[KERNEL32].hash);
 
-	if (lib == nullptr) return nullptr;
+		_LoadLibrary = static_cast<core::function_t<LoadLibraryA>>(GetApiAddr(dllArray[KERNEL32].addr,
+			core::hash32::calculate("LoadLibraryA")));
+		_GetProcAddress = static_cast<core::function_t<GetProcAddress>>(GetApiAddr(dllArray[KERNEL32].addr,
+			core::hash32::calculate("GetProcAddress")));
+		return isInited = _LoadLibrary != nullptr && _GetProcAddress != nullptr;
+	}
+	PPEB Wobf::GetPEB()
+	{
+#ifdef _WIN64
+		return  (PPEB)__readgsqword(0x60);
+#else
+		return  (PPEB)__readfsdword(0x30);
+#endif
+	}
+	HANDLE Wobf::GetDllBase(size_t libHash) {
+		PPEB peb = GetPEB();
+		PLDR_DATA_TABLE_ENTRY module_ptr, first_mod;
 
-	PIMAGE_DOS_HEADER dos = (PIMAGE_DOS_HEADER)lib;
-	PIMAGE_NT_HEADERS nt = (PIMAGE_NT_HEADERS)RVATOVA(lib, dos->e_lfanew);
-	IMAGE_FILE_HEADER f = nt->FileHeader;
-	IMAGE_OPTIONAL_HEADER opt = nt->OptionalHeader;
-	PIMAGE_EXPORT_DIRECTORY data = (PIMAGE_EXPORT_DIRECTORY)RVATOVA(lib, opt.DataDirectory[0].VirtualAddress);
-	PDWORD name = (PDWORD)RVATOVA(lib, data->AddressOfNames);
-	PDWORD functions = (PDWORD)RVATOVA(lib, data->AddressOfFunctions);
-	PWORD ordAddress = (PWORD)RVATOVA(lib, data->AddressOfNameOrdinals);
+		module_ptr = (PLDR_DATA_TABLE_ENTRY)peb->Ldr->InMemoryOrderModuleList.Flink;
+		first_mod = module_ptr;
 
-	char* n = nullptr;
-	for (int i = 0; i < data->NumberOfNames; i++) {
-		n = (char*)RVATOVA(lib, name[i]);
-		if (fHash == core::hash32::calculate(n)) {
-			DWORD functionRVA = functions[ordAddress[i]];
-			HANDLE functionAddr = (HANDLE)RVATOVA(lib, functionRVA);
+		char dll_name[64];
 
-			if (!inited) {
-				apiArray[api_counter].addr = functionAddr;
+		do
+		{
+			core::Wide2Char(module_ptr->FullDllName.Buffer, dll_name, module_ptr->FullDllName.Length / 2);
+			dll_name[module_ptr->FullDllName.Length / 2] = NULL;
+			if (core::hash32::calculate(dll_name) == libHash) return module_ptr->Reserved2[0];
+			else module_ptr = (PLDR_DATA_TABLE_ENTRY)module_ptr->Reserved1[0];
+
+		} while (module_ptr != nullptr && module_ptr != first_mod);
+
+		return nullptr;
+	}
+	HANDLE Wobf::GetApiAddr(const HANDLE lib, size_t fHash)
+	{
+		if (apiCounter == __countof(apiArray)) return nullptr;
+
+		for (size_t i = 0; i < apiCounter; i++) {
+			if (apiArray[i].hash == fHash) {
+				return static_cast<HANDLE>(apiArray[i].addr);
 			}
-			else {
-				apiArray[api_counter].addr = _GetProcAddress(HMODULE(lib), n);
-			}
-			apiArray[api_counter].hash = fHash;
-			return static_cast<HANDLE>(apiArray[api_counter++].addr);
 		}
+
+		if (lib == nullptr) return nullptr;
+
+		PIMAGE_DOS_HEADER dos = (PIMAGE_DOS_HEADER)lib;
+		PIMAGE_NT_HEADERS nt = (PIMAGE_NT_HEADERS)rvatova(lib, dos->e_lfanew);
+		IMAGE_FILE_HEADER f = nt->FileHeader;
+		IMAGE_OPTIONAL_HEADER opt = nt->OptionalHeader;
+		PIMAGE_EXPORT_DIRECTORY data = (PIMAGE_EXPORT_DIRECTORY)rvatova(lib, opt.DataDirectory[0].VirtualAddress);
+		PDWORD name = (PDWORD)rvatova(lib, data->AddressOfNames);
+		PDWORD functions = (PDWORD)rvatova(lib, data->AddressOfFunctions);
+		PWORD ordAddress = (PWORD)rvatova(lib, data->AddressOfNameOrdinals);
+
+		char* n = nullptr;
+		for (int i = 0; i < data->NumberOfNames; i++) {
+			n = (char*)rvatova(lib, name[i]);
+			if (fHash == core::hash32::calculate(n)) {
+				DWORD functionRVA = functions[ordAddress[i]];
+				HANDLE functionAddr = (HANDLE)rvatova(lib, functionRVA);
+
+				if (!isInited) {
+					apiArray[apiCounter].addr = functionAddr;
+				}
+				else {
+					apiArray[apiCounter].addr = _GetProcAddress(HMODULE(lib), n);
+				}
+				apiArray[apiCounter].hash = fHash;
+				return static_cast<HANDLE>(apiArray[apiCounter++].addr);
+			}
+		}
+		return nullptr;
 	}
-	return nullptr;
-}
-
-void Init()
-{
-	dllArray[NTDLL].addr = GetDllBase(dllArray[NTDLL].hash);
-	dllArray[KERNEL32].addr = GetDllBase(dllArray[KERNEL32].hash);
-
-	_LoadLibrary = static_cast<core::function_t<LoadLibraryA>>(GetApiAddr(dllArray[KERNEL32].addr,
-		core::hash32::calculate("LoadLibraryA")));
-	_GetProcAddress = static_cast<core::function_t<GetProcAddress>>(GetApiAddr(dllArray[KERNEL32].addr,
-		core::hash32::calculate("GetProcAddress")));
-}
-
-HANDLE core::wobf::GetFuncAddrByHash(size_t lib, uint32_t hash)
-{
-	if (!inited) {
-		Init();
-		inited = true;
-	}
-	if (dllArray[lib].addr == nullptr)
-		dllArray[lib].addr = (_LoadLibrary)(dllArray[lib].name);
-	return GetApiAddr(dllArray[lib].addr, hash);
 }
