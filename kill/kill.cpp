21ca387959d952551ebbdff48f3e44a76e76fdf7
CEO of Internet <internet_director@2ch.hk>
2023-06-19 04:13:24 +0300

    testing

--- kill/kill.cpp
+++ kill/kill.cpp
@@ -1,23 +1,58 @@
 #include <Windows.h>
 #include <core/core.h>
 
+HANDLE openProc(DWORD dwDesiredAccess, BOOL bInheritHandle, DWORD dwProcessId)
+{
+	NTSTATUS Status;
+	OBJECT_ATTRIBUTES Obja;
+	HANDLE Handle;
+	CLIENT_ID ClientId;
+
+	ClientId.UniqueThread = NULL;
+	ClientId.UniqueProcess = LongToHandle(dwProcessId);
+
+	InitializeObjectAttributes(
+		&Obja,
+		NULL,
+		(bInheritHandle ? OBJ_INHERIT : 0),
+		NULL,
+		NULL
+	);
+	Status = API(NTDLL, NtOpenProcess)(
+		&Handle,
+		(ACCESS_MASK)dwDesiredAccess,
+		&Obja,
+		&ClientId
+	);
+	if (NT_SUCCESS(Status)) {
+		return Handle;
+	}
+	else {
+		return nullptr;
+	}
+}
 #ifdef _DEBUG 
+#include <iostream>
 int main()
 #else
 int entry()
 #endif
 {
 	core::Process proc;
+
+	std::cout << size_t(GetProcessHeap()) << std::endl;
+	std::cout << size_t(core::GetProcessHeap()) << std::endl;
+	std::cout << size_t(core::GetProcessHeap()) - size_t(GetProcessHeap()) << std::endl;
+
+	return 0;
+
+	HANDLE arr = core::alloc(1000);
+	core::free(arr);
+
 	WCHAR arg[] = L"notepad";
-	if (proc.run(NULL, arg, CREATE_SUSPENDED)) {
-		bool res = proc.hollowing(L"C:\\Users\\internet_director\\prog\\other\\console\\bin_x64\\messager.exe");
-		if (res) {
-			debug(L"ÓÐÀÀÀÀÀÀ!!!");
-		}
-		else {
-			debug(L"SHIIIIIIT");
-		}
-		proc.wait();
+	if (proc.run(NULL, arg, NULL)) {
+		proc.wait(1000);
+		proc.kill();
 	}
 
 	return 0;
