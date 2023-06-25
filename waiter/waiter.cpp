bfd2e842c614f08726be5bccb0df91c5a5de887f
CEO of Internet <internet_director@2ch.hk>
2023-06-26 01:57:34 +0300

    just wait

--- waiter/waiter.cpp
+++ waiter/waiter.cpp
@@ -1,11 +1,15 @@
-#include <iostream>
 #include <Windows.h>
-#include <TlHelp32.h>
+#include <core/core.h>
+#include <iostream>
 
 int main()
 {
-	for (int i = 0; i < 1000000; i++) {
-		Sleep(1000000);
+	core::init();
+	{
+		for (int i = 0; i < 1000; i++) {
+			Sleep(100000);
+		}
 	}
+	core::close();
 	return 0;
 }
