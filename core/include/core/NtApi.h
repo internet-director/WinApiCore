4fd59e0804161634649be6011e7b9ca5ba348cf1
CEO of Internet <internet_director@2ch.hk>
2023-06-26 02:19:20 +0300

    support CreateFIleW

--- core/include/core/NtApi.h
+++ core/include/core/NtApi.h
@@ -17,6 +17,7 @@ namespace core {
 		static DWORD ResumeThread(HANDLE hThread);
 		static bool CloseHandle(HANDLE h);
 		static void RtlInitUnicodeString(PUNICODE_STRING DestinationString, PCWSTR SourceString);
+
 		static HANDLE GetProcessHeap();
 		static HANDLE CreateFileW(_In_ LPCWSTR lpFileName, _In_ DWORD dwDesiredAccess, _In_ DWORD dwShareMode,
 			_In_opt_ LPSECURITY_ATTRIBUTES lpSecurityAttributes, _In_ DWORD dwCreationDisposition, _In_ DWORD dwFlagsAndAttributes, _In_opt_ HANDLE hTemplateFile
@@ -59,6 +60,14 @@ namespace core {
 	inline bool CloseHandle(HANDLE h)
 	{
 		return core::NtApi::CloseHandle(h);
+	}
+
+
+	inline HANDLE CreateFileW(_In_ LPCWSTR lpFileName, _In_ DWORD dwDesiredAccess, _In_ DWORD dwShareMode,
+		_In_opt_ LPSECURITY_ATTRIBUTES lpSecurityAttributes, _In_ DWORD dwCreationDisposition, _In_ DWORD dwFlagsAndAttributes, _In_opt_ HANDLE hTemplateFile
+	) {
+		return core::NtApi::CreateFileW(lpFileName, dwDesiredAccess, dwShareMode,
+			lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile);
 	}
 
 	inline HANDLE GetProcessHeap()
